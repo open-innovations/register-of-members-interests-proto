70afa3f69ad20b22ca6ae06704c0600983fa2ada
Giles Dring <giles@dringtech.com>
2022-10-12 19:46:07 +0100

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import date from "lume/plugins/date.ts";
 
 const site = lume({
   src: "./src",
+  location: new URL('https://open-innovations.github.io/register-of-members-interests-proto/'),
   components: {
     cssFile: "/assets/css/components.css",
     jsFile: "/assets/js/components.js",
