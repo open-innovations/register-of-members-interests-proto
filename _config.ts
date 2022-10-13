677f9f5a0f83375a66c00be6aa95d615a4de7c65
Giles Dring <giles@dringtech.com>
2022-10-13 09:31:31 +0100

    Add popup for view

--- _config.ts
+++ _config.ts
@@ -1,7 +1,10 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
-import sheets from "lume/plugins/sheets.ts";
 import date from "lume/plugins/date.ts";
+import esbuild from 'lume/plugins/esbuild.ts';
+import sheets from "lume/plugins/sheets.ts";
+
+import autoDependency from 'oi-lume-utils/processors/auto-dependency.ts';
 
 const site = lume({
   src: "./src",
@@ -14,8 +17,23 @@ const site = lume({
 
 site.copy([".css"]);
 
+site.process(['.html'], autoDependency)
+
 site.use(basePath());
 site.use(date());
+site.use(esbuild({
+  extensions: [".ts", ".js"],
+  options: {
+    bundle: true,
+    format: "iife",
+    minify: true,
+    keepNames: false,
+    platform: "browser",
+    target: "es6",
+    incremental: true,
+    treeShaking: true,
+  },
+}));
 site.use(sheets());
 
 export default site;
