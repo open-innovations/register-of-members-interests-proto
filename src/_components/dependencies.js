59472b5b51b5b609b1b0c8c9e6f1881b025f4862
Giles Dring <giles@dringtech.com>
2022-11-23 13:07:22 +0000

    Add calculability

---
+++ src/_components/dependencies.js
@@ -0,0 +1,20 @@
+export default function ({ id, current_state: currentState, competency, states }) {
+  const state = states[currentState];
+  if (state === undefined) throw 'Unknown state';
+
+  let result = '';
+
+  const dependencies = competency[id]?.dependencies || [];
+  result += 'data-dependencies="' + dependencies + '"';
+
+  const features = Object.keys(state.available);
+  const score = dependencies.length > 0
+    ?
+    dependencies.map(dependency => features.includes(dependency) && 1 || 0).reduce((total, current) => total * current, 1)
+    :
+    undefined;
+  if (score !== undefined) result += ' data-score="' + score + '"';
+
+  return result;
+}
+
