b6618ab559b7f76058cecde07313f6fa50d6c2f8
Giles Dring <giles@dringtech.com>
2022-11-30 19:29:36 +0000

    Clean up site

--- src/state.tmpl.js
+++ src/state.tmpl.js
@@ -3,10 +3,11 @@ export const tags = ["state"];
 
 export default function* ({ states }) {
   for (const [state, config] of Object.entries(states)) {
-    yield {
-      ...config,
-      current_state: state,
-      url: `/state/${state}/`,
-    };
+    if (!config?.draft)
+      yield {
+        ...config,
+        current_state: state,
+        url: `/state/${state}/`,
+      };
   }
 }
