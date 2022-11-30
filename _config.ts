2eb34b5d939aae6c64773f4c502e066c9cff2d6a
Giles Dring <giles@dringtech.com>
2022-11-30 19:12:11 +0000

    Add SEO

--- _config.ts
+++ _config.ts
@@ -1,14 +1,17 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import date from "lume/plugins/date.ts";
-import esbuild from 'lume/plugins/esbuild.ts';
+import esbuild from "lume/plugins/esbuild.ts";
+import metas from "lume/plugins/metas.ts";
 import sheets from "lume/plugins/sheets.ts";
 
-import autoDependency from 'oi-lume-utils/processors/auto-dependency.ts';
+import autoDependency from "oi-lume-utils/processors/auto-dependency.ts";
 
 const site = lume({
   src: "./src",
-  location: new URL('https://open-innovations.github.io/register-of-members-interests-proto/'),
+  location: new URL(
+    "https://open-innovations.github.io/register-of-members-interests-proto/",
+  ),
   components: {
     cssFile: "/assets/css/components.css",
     jsFile: "/assets/js/components.js",
@@ -17,12 +20,15 @@ const site = lume({
 
 site.copy([".css"]);
 
-site.process(['.html'], autoDependency)
+site.process([".html"], autoDependency);
 
-function objectToList(o: { [k: string]: object }, id_field: string = 'id') {
-  return Object.entries(o).map(([k, v]) => ({ [id_field]: k, ...v }))
+function objectToList(
+  o: { [k: string]: Record<string, unknown> },
+  id_field = "id",
+) {
+  return Object.entries(o).map(([k, v]) => ({ [id_field]: k, ...v }));
 }
-site.filter('listify', objectToList)
+site.filter("listify", objectToList);
 
 site.use(basePath());
 site.use(date());
@@ -40,5 +46,10 @@ site.use(esbuild({
   },
 }));
 site.use(sheets());
+site.use(metas({
+  defaultPageData: {
+    title: "title", // Use the `title` value as fallback.
+  },
+}));
 
 export default site;
