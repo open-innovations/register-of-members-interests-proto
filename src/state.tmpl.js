ddcbc59874f31b20fbb015ade7c9b0371ce4a997
Giles Dring <giles@dringtech.com>
2022-11-30 17:52:03 +0000

    Add state navigator

--- src/state.tmpl.js
+++ src/state.tmpl.js
@@ -2,9 +2,10 @@ export const layout = 'layouts/state.njk';
 export const tags = ['state'];
 
 export default function*({ states }) {
-  for (const state of Object.keys(states)) {
+  for (const [state, config] of Object.entries(states)) {
     console.log(state);
     yield {
+      ...config,
       current_state: state,
       url: `/state/${state}/`,
     };
