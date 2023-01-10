589c11d8c529d44f181fb8b4cfb656fe11d51633
Giles Dring <giles@dringtech.com>
2023-01-10 12:43:36 +0000

    Make name of imagined features more meaningful

--- scripts/randomly_assign_imagined_features.py
+++ scripts/randomly_assign_imagined_features.py
@@ -4,34 +4,39 @@ import hashlib
 
 COMPETENCY_DIR = 'src/_data/competency'
 
+
 def random_shuffle(digit, threshold):
     return int(digit, 16) < threshold
+
 
 def remove_items(list, values):
     for value in values:
         while value in list:
             list.remove(value)
 
+
 if __name__ == "__main__":
     competencies = [os.path.join(COMPETENCY_DIR, f)
                     for f in os.listdir('src/_data/competency')]
     for f in competencies:
-      hash = hashlib.md5(f.encode()).hexdigest()
-      with open(f, 'r', encoding="utf-8") as stream:
-          data = yaml.load(stream, Loader=yaml.Loader) or {}
-      if 'dependencies' not in data:
-          data['dependencies'] = []
+        hash = hashlib.md5(f.encode()).hexdigest()
+        with open(f, 'r', encoding="utf-8") as stream:
+            data = yaml.load(stream, Loader=yaml.Loader) or {}
+        if 'dependencies' not in data:
+            data['dependencies'] = []
 
-      remove_items(data['dependencies'], ['imagined.current', 'imagined.future', 'imagined.unlikely'])
-      random_shuffle(hash[0], 10) and data['dependencies'].append('imagined.current')
-      random_shuffle(hash[2], 5) and data['dependencies'].append('imagined.future')
-      random_shuffle(hash[3], 2) and data['dependencies'].append('imagined.unlikely')
+        remove_items(data['dependencies'], ['imagined.feature.current',
+                     'imagined.feature.future', 'imagined.feature.unlikely'])
+        random_shuffle(hash[0], 10) and data['dependencies'].append(
+            'imagined.feature.current')
+        random_shuffle(hash[2], 5) and data['dependencies'].append(
+            'imagined.feature.future')
+        random_shuffle(hash[3], 2) and data['dependencies'].append(
+            'imagined.feature.unlikely')
 
-      # Make this unique
-      data['dependencies'] = list(set(data['dependencies']))
-      data['dependencies'].sort()
+        # Make this unique
+        data['dependencies'] = list(set(data['dependencies']))
+        data['dependencies'].sort()
 
-      with open(f, 'w', encoding="utf-8") as stream:
-          stream.write(yaml.dump(data))
-
-      
+        with open(f, 'w', encoding="utf-8") as stream:
+            stream.write(yaml.dump(data))
