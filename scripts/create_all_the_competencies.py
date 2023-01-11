72402a5cbbede1aef0db7bd25b457389956aa309
Giles Dring <giles@dringtech.com>
2023-01-11 12:01:59 +0000

    De-duplicate competencies based on evidence.csv

--- scripts/create_all_the_competencies.py
+++ scripts/create_all_the_competencies.py
@@ -6,7 +6,8 @@ from pathlib import Path
 EVIDENCE_CSV ='src/_data/evidence.csv'
 
 if __name__ == "__main__":
-    competencies = pd.read_csv(EVIDENCE_CSV, usecols=['ID', 'Competency', 'Duplicates'], na_values=[''])
+    competencies = pd.read_csv(EVIDENCE_CSV, usecols=['ID', 'Competency', 'Duplicates'])
+    competencies.Duplicates = competencies.Duplicates.fillna('').str.replace(' ', '')
 
     for index, competency in competencies.iterrows():
         file_path = os.path.join('src', '_data', 'competency', competency.ID + '.yml')
@@ -16,8 +17,9 @@ if __name__ == "__main__":
                 data = yaml.load(stream, Loader=yaml.Loader) or {}
         except FileNotFoundError:
             data = {}
-
+        
         data['competency'] = competency.Competency
-
+        data['duplicate_of'] = [x for x in competency.Duplicates.split('/') if x != '']
+  
         with open(file_path, 'w', encoding="utf-8") as stream:
             stream.write(yaml.dump(data))
